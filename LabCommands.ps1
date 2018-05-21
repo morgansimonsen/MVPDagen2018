894fb0ac11c5cea551fbc7f99149840989a8215c
morgansimonsen <morgan@simonsen.bz>
2018-05-21 23:42:57 +0200

    update demo files

---
+++ LabCommands.ps1
@@ -0,0 +1,8 @@
+# Download WAC
+Start-BitsTransfer -Source "http://download.microsoft.com/download/1/0/5/1059800B-F375-451C-B37E-758FFC7C8C8B/WindowsAdminCenter1804.25.msi"
+# Import cert
+Import-PfxCertificate -FilePath C:\Users\Administrator.HOME-HYPERV1\Downloads\home-hyperv1.pfx `
+                        -Exportable -Password ((Get-Credential).password) `
+                        -CertStoreLocation Cert:\LocalMachine\My
+# Install WAC
+msiexec /i WindowsAdminCenter1804.25.msi /qn /L*v log.txt SME_PORT=6516 SME_THUMBPRINT=72245DF7DE63A2280856164BBDFE027E2EFBECD6 SSL_CERTIFICATE_OPTION=installed
--- install_packer.sh
+++ install_packer.sh
@@ -2,3 +2,4 @@ cd ~
 curl https://releases.hashicorp.com/packer/1.2.3/packer_1.2.3_linux_amd64.zip -O -J -L
 unzip ./packer_1.2.3_linux_amd64.zip -d /usr/local/packer
 export PATH=$PATH:/usr/local/packer
+# sudo bash ./git/morgansimonsen/MVPDagen2018/install_packer.sh

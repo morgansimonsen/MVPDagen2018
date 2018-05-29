7ae13a8887c416aefb39209df9d09c147f01ccc7
Morgan Simonsen <morgan@simonsen.bz>
2018-05-30 01:46:26 +0200

    more demo file updates

--- wac/Enable-WACRemoteManagementAzureVM.ps1
+++ wac/Enable-WACRemoteManagementAzureVM.ps1
@@ -1,5 +1,7 @@
 <#
 These steps are necessary to allow WAC to connect to and manage an Azure IaaS VM
+https://docs.microsoft.com/en-us/windows-server/manage/windows-admin-center/configure/azure-integration
 #>
+Set-NetFirewallRule -Name WINRM-HTTP-In-TCP-PUBLIC -RemoteAddress Any
 winrm quickconfig -quiet -force
 Set-Item WSMan:localhost\Client\TrustedHosts -Value '10.1.0.4,ls-wac1.langskip.no,ls-wac1' -Force
--- wac/Install-WAC.ps1
+++ wac/Install-WAC.ps1
@@ -3,7 +3,15 @@ Install-WAC.ps1
 Morgan Simonsen
 #>
 
+# import certificate
 Import-PfxCertificate -Exportable:$true -FilePath c:\windows\temp\ls-wac1.pfx -CertStoreLocation Cert:\LocalMachine\My -Password (Get-Credential).password
+
+# install net fw
+$netfwdownloadurl = "https://download.microsoft.com/download/3/3/2/332D9665-37D5-467A-84E1-D07101375B8C/NDP472-KB4054531-Web.exe"
+$netfwInstallerFileBaseName = $netfwdownloadurl.Substring($netfwdownloadurl.LastIndexOf("/") + 1)
+$netfwInstallerFileFullName = Join-Path -Path $WACInstallerDestinationFolder -ChildPath $netfwInstallerFileBaseName
+Start-BitsTransfer -Source $netfwdownloadurl -Destination $WACInstallerDestinationFolder
+Start-Process -FilePath $netfwInstallerFileFullName -ArgumentList "/q /norestart" -Wait
 
 $WACInstallerDestinationFolder = "c:\windows\temp"
 $WACDownloadUrl = "http://download.microsoft.com/download/1/0/5/1059800B-F375-451C-B37E-758FFC7C8C8B/WindowsAdminCenter1804.25.msi"
@@ -25,3 +33,5 @@ $MSIArguments = @(
     $logFile
 )
 Start-Process "msiexec.exe" -ArgumentList $MSIArguments -Wait -NoNewWindow 
+
+Add-WindowsFeature -Name RSAT-Storage-Replica
